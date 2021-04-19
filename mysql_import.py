1a32a2541521b7228284dc39071adcef301e490e
Lucas Facchini <lucas.cf@live.com>
2021-04-18 23:50:35 -0300

    Fix in directory argument

--- mysql_import.py
+++ mysql_import.py
@@ -13,17 +13,23 @@ def parse_args():
 
         exit()
 
-    return {
+    args = {
         'host': sys.argv[1],
         'port': sys.argv[2],
         'user': sys.argv[3],
         'password': sys.argv[4],
-        'database': sys.argv[5],
-        'directory': sys.argv[6]
+        'database': sys.argv[5]
     }
 
+    if len(sys.argv) > 6:
+        args['directory'] = sys.argv[6]
+    else:
+        args['directory'] = DEFAULT_DIRECTORY
+
+    return args
+
+
 args = parse_args()
-directory = args['directory'] if args['directory'] else DEFAULT_DIRECTORY
 log = Log()
 sql = MysqlImport(args['host'], args['port'], args['user'], args['password'], args['database'], log)
 log.info('Creating schema')
@@ -31,7 +37,7 @@ sql.run_script('schema/mysql/drop-tables.sql')
 sql.run_script('schema/mysql/create-tables.sql')
 
 log.info('Analyzing files')
-parsers = generate_parsers_from_files(directory, log)
+parsers = generate_parsers_from_files(args['directory'], log)
 
 if len(parsers) > 0:
     log.info('Found', len(parsers), 'files')
